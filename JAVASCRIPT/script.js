905e1fb1654c57c969411cf806b01a7fe519b5ab
NitinKumar1054 <nitin2911.in@gmail.com>
2026-07-02 13:29:27 +0530

    Arrays , break, continue in js

--- JAVASCRIPT/script.js
+++ JAVASCRIPT/script.js
@@ -1,14 +1,57 @@
-for(i = 1; i<=10; i++){
+// for(i = 1; i<=10; i++){
+//     console.log(i);
+//     console.log("hello world");
+// }
+// i =1
+// while(i<=10){
+//     console.log('A' +i);
+//     i++;
+// }
+// i =1
+// do{
+//     console.log("Hello World");
+//     i++;
+// }while(i<=5);
+
+
+
+let students = ["Nitin", "Ram", "Rahul", "Krishna"];
+for(i = 0; i<students.length; i++){
+    console.log(students[i]);
+  
+   
+}
+  students[1] = "xyz";
+ console.log(students);
+ students.push("Anjali");
+ console.log(students);
+ students.unshift("Anamika");
+ console.log(students);
+ students.pop()
+ console.log(students);
+ students.shift();
+ console.log(students);
+ let numbers = [1,2,3,4,5,6,7,8,9];
+ for(let i = 0; i<numbers.length; i++){
+    if(numbers[i]%2 ===0){
+        continue;
+        console.log(`${numbers[i]} is a even number`);
+        
+
+    }else{
+        console.log(`${numbers[i]} is an odd number`);
+    }
+ }
+ for(let number of numbers){
+     if(number%2 ===0){
+        console.log(`${number} is a even number`);
+        break;
+
+    }else{
+        console.log(`${number} is an odd number`);
+    }
+
+ }
+ for(let i in numbers){
     console.log(i);
-    console.log("hello world");
-}
-i =1
-while(i<=10){
-    console.log('A' +i);
-    i++;
-}
-i =1
-do{
-    console.log("Hello World");
-    i++
-}while(i<=5)
+ }
