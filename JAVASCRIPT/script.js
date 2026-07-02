e37d13bc32452fbb15aa3101800b219c7dc1e4df
NitinKumar1054 <nitin2911.in@gmail.com>
2026-07-02 12:46:00 +0530

    while and do while loop in js

--- JAVASCRIPT/script.js
+++ JAVASCRIPT/script.js
@@ -2,3 +2,13 @@ for(i = 1; i<=10; i++){
     console.log(i);
     console.log("hello world");
 }
+i =1
+while(i<=10){
+    console.log('A' +i);
+    i++;
+}
+i =1
+do{
+    console.log("Hello World");
+    i++
+}while(i<=5)
