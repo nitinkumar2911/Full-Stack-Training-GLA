dd4d17b94ca483a8389599e7f54c7f6b628b3487
NitinKumar1054 <nitin2911.in@gmail.com>
2026-07-03 09:15:02 +0530

    Practical 2

--- JAVASCRIPT/practical.js
+++ JAVASCRIPT/practical.js
@@ -1,2 +1,15 @@
-console.log('My first Javascript file is connected!');
-alert('Welcome to Javascript, ' + 'Nitin Kumar');
+// console.log('My first Javascript file is connected!');
+// console.log("Nitin")
+// alert('Welcome to Javascript, ' + 'Nitin Kumar');
+
+const fullName = 'Nitin Kumar';
+fullName = "RAm"
+let age = 0;
+const isStudent = true;
+let favouriteColor ;
+const pet = null;
+console.log(`Name: ${fullName}, type: ${typeof fullName}`);
+console.log(`age: ${age}, type: ${typeof age}`);
+console.log(`isStudent: ${isStudent}, type: ${typeof isStudent}`);
+console.log(`favouriteColor: ${favouriteColor}, type: ${typeof favouriteColor}`);
+console.log(` pet: ${ pet}, type: ${typeof  pet}`);
