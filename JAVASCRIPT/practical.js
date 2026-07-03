aba14afc2d00cb789b50dbb57b638990511f5730
NitinKumar1054 <nitin2911.in@gmail.com>
2026-07-03 09:23:30 +0530

    practical 3

--- JAVASCRIPT/practical.js
+++ JAVASCRIPT/practical.js
@@ -2,14 +2,27 @@
 // console.log("Nitin")
 // alert('Welcome to Javascript, ' + 'Nitin Kumar');
 
-const fullName = 'Nitin Kumar';
-fullName = "RAm"
-let age = 0;
-const isStudent = true;
-let favouriteColor ;
-const pet = null;
-console.log(`Name: ${fullName}, type: ${typeof fullName}`);
-console.log(`age: ${age}, type: ${typeof age}`);
-console.log(`isStudent: ${isStudent}, type: ${typeof isStudent}`);
-console.log(`favouriteColor: ${favouriteColor}, type: ${typeof favouriteColor}`);
-console.log(` pet: ${ pet}, type: ${typeof  pet}`);
+// const fullName = 'Nitin Kumar';
+// fullName = "RAm"
+// let age = 0;
+// const isStudent = true;
+// let favouriteColor ;
+// const pet = null;
+// console.log(`Name: ${fullName}, type: ${typeof fullName}`);
+// console.log(`age: ${age}, type: ${typeof age}`);
+// console.log(`isStudent: ${isStudent}, type: ${typeof isStudent}`);
+// console.log(`favouriteColor: ${favouriteColor}, type: ${typeof favouriteColor}`);
+// console.log(` pet: ${ pet}, type: ${typeof  pet}`);
+const num1 = 25;
+const num2 = 4;
+console.log(`Sum: ${num1 + num2}`);
+console.log(`Difference: ${num1 - num2}`);
+console.log(`Product: ${num1 * num2}`);
+console.log(`Quotient: ${num1 / num2}`);
+console.log(`Remainder: ${num1 % num2}`);
+console.log(`Power: ${num1 ** 2}`);
+console.log(num1 > num2); 
+console.log('5' === 5);
+ console.log(10/3);
+ let a =parseInt("100px");
+ console.log(a+50);
