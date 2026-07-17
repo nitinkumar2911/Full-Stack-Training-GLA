7369f4d738560ab8ad1b3190d23a9fd897c56f2d
NitinKumar1054 <nitin2911.in@gmail.com>
2026-07-17 13:43:50 +0530

    React basic with jsx

--- REACT-CLASS/app.js
+++ REACT-CLASS/app.js
@@ -1,26 +1,25 @@
-const card = React.createElement("div",{style:{
-    border: "2px solid black",
-    padding: "20px",
-    width: "300px",
-    borderRadius: "10px",
-    textAlign:"center",
-    backgroundColor:"#f4f4fr4"
+const card = (
+    <div
+        style={{
+            border: "2px solid black",
+            padding: "20px",
+            width: "300px",
+            borderRadius: "10px",
+            textAlign: "center",
+            backgroundColor: "#f4f4f4"
+        }}
+    >
+        <h1>Nitin Kumar</h1>
 
-}},
-React.createElement(
-    "h1",
-    null,
-    "Nitin Kumar"
-),
-React.createElement(
-    "p",
-    null,
-    "Frontend Developer"
-),
-React.createElement("button",{
-    onClick: function(){
-        alert("Welcome Nitin!");
-    }
-}, "Click Me"));
+        <p>Frontend Developer</p>
+
+        <button
+            onClick={() => alert("Welcome Nitin!")}
+        >
+            Click Me
+        </button>
+    </div>
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(card);
