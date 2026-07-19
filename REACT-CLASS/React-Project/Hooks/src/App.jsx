f71cccfc10f6ce0c58f55af06ad791eff54ceef5
NitinKumar1054 <nitin2911.in@gmail.com>
2026-07-19 10:09:50 +0530

    Understanding useEffect in react

--- REACT-CLASS/React-Project/Hooks/src/App.jsx
+++ REACT-CLASS/React-Project/Hooks/src/App.jsx
@@ -1,36 +1,21 @@
-import { useState } from 'react'
+import { useState, useEffect} from 'react'
 
 
 function App() {
-  const [color, setColor] = useState("red");
-  const [car, setCar] = useState({
-    brand: "Ford",
-    model: "Mustang",
-    year: "1964",
-    color: "red"
-  });
-  const updateColor = () => {
-    setCar(previousState => {
-      return { ...previousState, color: "blue" }
-    });
-  }
+  const [count, setCount] = useState(0);
+  const [calculation, setCalculation] = useState(0);
+
+  useEffect(() => {
+    setCalculation(() => count * 2);
+  }, [count]);
 
 
   return (
     <>
-     <h1>My favorite color is {color}!</h1>
-      <button
-        type="button"
-        onClick={() => setColor("blue")}
-      >Blue</button>
-       <h1>My {car.brand}</h1>
-      <p>
-        It is a {car.color} {car.model} from {car.year}.
-      </p>
-      <button
-        type="button"
-        onClick={updateColor}
-      >Blue</button>
+    <p>Count: {count}</p>
+      <button onClick={() => setCount((c) => c + 1)}>+</button>
+      <p>Calculation: {calculation}</p>
+      
     </>
   )
 }
